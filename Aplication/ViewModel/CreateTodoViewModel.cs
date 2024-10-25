080f4134851a064edcb8e32f6cbc7fcfb007806c
Vinicius Dias <diasvinicius95@outlook.com>
2024-10-25 15:03:16 -0300

    feat: adicionando infraestrutura de base para arquitetura

---
+++ Aplication/ViewModel/CreateTodoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApi.ViewModel
+{
+    public class CreateTodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+    }
+}
